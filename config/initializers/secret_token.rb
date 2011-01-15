2ce7bb58c65d00a5fd1c05042ad0ad0023122440
Marco Gonzalo Gomez Perez <marcogonzalo@gmail.com>
2011-01-15 14:31:07 -0430

    proyecto rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabatiMG::Application.config.secret_token = '2faa2cf6358af501ed5e03adfb62ff7e250c5b3303dcfabba0a4e15e3d205506dd3fd7a0783d879dbb1f1c7b86c19e553a9954db91a7bb41111d7b036cb03960'
